d786836c19b1039b14c9ceb1fdaeda49a3bad0de
Henry Boyd <AmorphousCube@gmail.com>
2020-03-02 20:17:01 +0000

    added exercises 1-4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,11 @@
 require_relative '../setup'
 
+Burnaby = Store.create(name: "Burnaby", annual_revenue:300000, mens_apparel:true, womens_apparel:true)
+Richmond = Store.create(name:"Richmond", annual_revenue:1260000, mens_apparel:false, womens_apparel:true)
+Gsstown = Store.create(name:"Gastown", annual_revenue:190000, mens_apparel:true, womens_apparel:false)
+
+
 puts "Exercise 1"
-puts "----------"
+puts Store.count
 
 # Your code goes below here ...
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -3,7 +3,20 @@ require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 
+Surrey = Store.create(name: "Surrey", annual_revenue:224000, mens_apparel:false, womens_apparel:true)
+Whistler = Store.create(name:"Richmond", annual_revenue:1900000, mens_apparel:true, womens_apparel:false)
+Yaletown = Store.create(name:"Yaletown", annual_revenue:430000, mens_apparel:true, womens_apparel:true)
+
+@mens_stores = Store.where(mens_apparel:true)
+@womens_stores_under_1M = Store.where('annual_revenue < 1000000', womens_apparel:true)
+
 puts "Exercise 4"
+@mens_stores.each do |element|
+  puts "#{element.name} has annual revenue of #{element.annual_revenue}"
+end
+@womens_stores_under_1M.each do |element|
+  puts "#{element.name} has annual revenue of #{element.annual_revenue}"
+end
 puts "----------"
 
 # Your code goes here ...
